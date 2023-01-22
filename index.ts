aade1915737d4995f636f5224a7a0e5ba09162c0
Bryan Ochoa <bochoatrillo@gmail.com>
2023-01-22 12:41:11 -0500

    Traslada las configuraciones a app.ts

--- index.ts
+++ index.ts
@@ -1,50 +1,6 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-
-//Importando prisma client
-import { PrismaClient } from '@prisma/client';
-
-dotenv.config();
-
-//iniciando prisma client
-const prisma = new PrismaClient();
-
-const app: Express = express();
+import { app } from './app';
 const PORT = process.env.PORT ||3000;
-
-app.use(express.json());
-
-app.get('/', (req:Request, res:Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-
-//rutas db
-app.post("/author", async(req: Request,res: Response) => {
-  const { name, email } = req.body;
-  const user = await prisma.user.create({
-    data: {
-      name: name,
-      email: email
-    }
-  });
-  res.json(user)
-});
-
-app.post("/post", async (req, res) => {
-  const { title, content, author } = req.body;
-  const result = await prisma.post.create({
-    data: {
-      title: title,
-      content: content,
-      author: { connect: { id: author } },
-    }
-  });
-  res.json(result)
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
-
-
