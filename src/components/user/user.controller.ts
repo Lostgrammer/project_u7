15a5b8d7dfef3613e29ede24f9e21ae9f3f8d97f
Bryan Ochoa <bochoatrillo@gmail.com>
2023-01-24 14:09:32 -0500

    Actualiza el login para generar token de acceso

--- src/components/user/user.controller.ts
+++ src/components/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { prisma } from '../../app';
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
+import { generateToken } from "../../middlewares/auth";
 
 export const registerUser =async (req: Request, res: Response) => {
     try {
@@ -24,18 +25,38 @@ export const registerUser =async (req: Request, res: Response) => {
 export const loginUser = async (req: Request, res: Response) => {
     try {
         const { body } = req;
-        const user = await prisma.user.findFirst({
+        let user = await prisma.user.findFirst({
             where: {
-                email: body.email,
-                AND: {
-                    password: body.password,
-                }
+                email: body.email
             },
         });
-        res.status(200).json({
-            ok: true,
-            data: user
-        });
+        if (user && user.password == body.password) {
+            const currentTime = new Date();
+            user = await prisma.user.update({
+                where: {
+                    id: user.id
+                },
+                data: {
+                    last_session: 
+                        currentTime.toISOString()
+                        .replace(/\d\d:\d\d:\d\d/,currentTime.toLocaleTimeString())
+                        
+                }
+            })
+            res.status(200).json({
+                ok: true,
+                data: {
+                    id: user.id,
+                    email: user.email,
+                    name: user.name,
+                    date_born: user.date_born,
+                    last_session: user.last_session,
+                    token: generateToken(user.id)
+                }
+            });
+        } else {
+            throw new Error("Invalid email or password");
+        }
     } catch (error: any){
         return res.status(500).json({
             ok: false,
